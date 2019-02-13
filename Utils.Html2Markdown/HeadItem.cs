2a9450f14688b690c690f68e7816f62b5743421c
David malý <malyce.dvd@gmail.com>
2019-02-13 11:14:08 +0100

    Use head files instead of raw files location

---
+++ Utils.Html2Markdown/HeadItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils.Html2Markdown
+{
+    class HeadItem
+    {
+        public string Title { get; set; }
+        public string Name { get; set; }
+
+        public string NameWithoutExtension
+        {
+            get
+            {
+                if (Name.Contains("."))
+                {
+                    var name = Name.Split('.');
+                    return name[0];
+                }
+                return Name;
+            }
+        }
+    }
+}
--- Utils.Html2Markdown/Program.cs
+++ Utils.Html2Markdown/Program.cs
@@ -1,10 +1,16 @@
-﻿using Html2Markdown;
+﻿using ReverseMarkdown;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Utils.Html2Markdown
 {
+    // TODO LINQ
+    // TODO YIELD
     class Program
     {
         static async Task Main(string[] args)
@@ -18,31 +24,93 @@ namespace Utils.Html2Markdown
         }
 
         static string HTML2Markdown(string html)
-        { 
-            var converter = new Converter();
-            return converter.Convert(html);
-        }
-        
-        static async Task ConvertFiles(string path,string folder, string search)
         {
-            var dirs = Directory.GetFiles($"{path}\\{folder}", search, SearchOption.AllDirectories);
-            Console.WriteLine(folder);
-            Console.WriteLine();
-            foreach (var dirInfo in dirs)
+            var config = new ReverseMarkdown.Config
             {
-                Console.WriteLine(dirInfo);
-                System.IO.FileInfo fi = new System.IO.FileInfo(dirInfo);
-                if (fi.Name.Equals("index.php")) continue;
-                
-                string date = DateTime.Now.ToString("yyyy-MM-dd");
-                string parent = Directory.GetParent(dirInfo).Name;
+                UnknownTags = Config.UnknownTagsOption.PassThrough,
+                GithubFlavored = true,
+                SmartHrefHandling = true // remove markdown output for links where appropriate
+            };
 
-                var markdown = HTML2Markdown(await File.ReadAllTextAsync(fi.FullName));
+            var converter = new ReverseMarkdown.Converter(config);
+            string result = converter.Convert(html);
 
-                Directory.CreateDirectory($"C:\\git\\school-materials-export\\{folder}\\{parent}");
-                File.WriteAllText($"C:\\git\\school-materials-export\\{folder}\\{parent}\\{date}-{Path.GetFileNameWithoutExtension(fi.Name)}" + ".md", markdown);
+            return result;
+        }
+
+
+     
+
+        static async Task ConvertFiles(string path, string folder, string search)
+        {
+            var fullPath = $"{path}\\{folder}\\article";
+            
+           // DirectoryInfo[] dirs = di.GetDirectories();
+
+            Console.WriteLine();
+            Console.WriteLine(folder);
+
+            List<HeadItem> topHeadItems = await parseHead(fullPath);
+
+
+            Console.WriteLine();
+            foreach (var topHeadItem in topHeadItems)
+            {
+                Console.WriteLine(topHeadItem.Name);
+                DirectoryInfo di = new DirectoryInfo(Path.Combine(fullPath, topHeadItem.Name));
+                var files = di.GetFiles();
+
+                List<HeadItem> headItems = await parseHead(Path.Combine( fullPath, topHeadItem.Name) );
+
+
+                foreach (var fInfo in files)
+                {
+                    if (fInfo.Name.Equals("index.php")) continue;
+                    
+                    // find all files with name from head and create correct header
+
+                    var matchingHeadItem = from headItem in headItems
+                                               where headItem.NameWithoutExtension == Path.GetFileNameWithoutExtension(fInfo.Name)
+                                               select headItem;
+
+                    if (matchingHeadItem.FirstOrDefault() == null) continue;
+                    Console.WriteLine(matchingHeadItem.FirstOrDefault().Name);
+
+                    string date = DateTime.Now.ToString("yyyy-MM-dd");
+
+                    var markdown = HTML2Markdown(await File.ReadAllTextAsync(fInfo.FullName));
+
+                    string header = $@"---
+layout: post
+title: ""{ matchingHeadItem.FirstOrDefault().Name} ""
+categories:
+    -""{folder}""
+    -""{folder}""
+author: ""David Malý""
+--- 
+
+";
+                    Directory.CreateDirectory($"C:\\git\\school-materials-export\\{folder}\\{topHeadItem.Name}");
+                    File.WriteAllText($"C:\\git\\school-materials-export\\{folder}\\{topHeadItem.Name}\\{date}-{Path.GetFileNameWithoutExtension(fInfo.Name)}" + ".md", header + markdown);
+                }
 
             }
+        }
+
+        static async Task<List<HeadItem>> parseHead(string path)
+        {
+            List<HeadItem> headItems = new List<HeadItem>();
+            var lines = await File.ReadAllLinesAsync(Path.Combine(path, "head.txt"));
+            foreach(var line in lines)
+            {
+                var splited = line.Split(";");
+                headItems.Add(new HeadItem
+                {
+                    Title = splited[0],
+                    Name = splited[1]
+                });
+            }
+            return headItems;
         }
     }
 }
